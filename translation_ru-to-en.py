946782430e74ae3e6e182151ff6101b25e694597
AlekseyChurilov <chualek@mail.ru>
2022-12-09 23:42:25 +0300

    Added file translation_ru-to-en

---
+++ translation_ru-to-en.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+
+translator = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
+
+translator("Вчера вечером я отдыхал в баре, а сегодня я должен учиться, но у меня болит голова")~
+~
+~
+~
+~
+~
+~
+~
+~
